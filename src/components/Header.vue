b5a149d93f077ca56f2747b9e20c79361a9ab1cc
hesh ramsis <heshamelmasry1992@gmail.com>
2022-10-22 21:20:42 +0200

    add tailwind

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import {FaHome} from "oh-vue-icons/icons";
+
+export default {
+  props: [],
+  components: {
+    FaHome
+  },
+  data() {
+    return {}
+  },
+}
+</script>
